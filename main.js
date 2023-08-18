c91ab3ff7f96567b8b0d91507e27def0345fd45b
jvictormleao <joaovictormarianoleao@gmail.com>
2023-08-18 10:27:24 -0300

    Language selection

--- main.js
+++ main.js
@@ -1,15 +1,27 @@
-const checkbox = document.querySelector("#language")
+//const checkbox = document.querySelector("#language")
 const pt = document.querySelector(".curriculo-area-pt")
 const eng = document.querySelector(".curriculo-area-eng")
 
-checkbox.addEventListener('change', function () {
+
+function setLanguage(){
+    checkbox = document.getElementById('language')
+    if (checkbox.checked){
+        console.log("Check")
+    pt.classList.toggle("hidden")
+    eng.classList.toggle("hidden")
+    } else {
+        pt.classList.toggle("hidden")
+    eng.classList.toggle("hidden")
+    }
+}
+/*checkbox.addEventListener('change', function () {
     if(this.checked === true){
         console.log("Check")
     pt.classList.remove("hidden")
     eng.classList.add("hidden")
     } else {
         console.log("Uncheck")
-        pt.classList.toggle("hidden")
-        eng.classList.toggle("hidden")
+    pt.classList.add("hidden")
+    eng.classList.remove("hidden")
     }
-})
+})*/
